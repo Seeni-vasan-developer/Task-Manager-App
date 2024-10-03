0e14888e5efc79449356b2c483bf9091b0ad90bc
Seeni-vasan-developer <nseenivasan129@gmail.com>
2024-10-03 20:48:51 +0530

    third commit

--- myproject/taskapp/urls.py
+++ myproject/taskapp/urls.py
@@ -2,9 +2,15 @@ from django.urls import path
 from taskapp import views
 
 urlpatterns = [
-    path('',views.view_all_tasks,name='view_all_tasks'),
-    path('add_task/',views.add_task,name='add_task'),
-    path('edit_task/<int:task_id>/',views.edit_task,name='edit_task'),
-    path('delete_task/<int:task_id>/',views.delete_task,name='delete_task'),
-    path('task1/',views.task1,name="task1"),
+    # path('task1/',views.view_all_tasks,name='view_all_tasks'),
+    # path('add_task/',views.add_task,name='add_task'),
+    # path('edit_task/<int:task_id>/',views.edit_task,name='edit_task'),
+    # path('delete_task/<int:task_id>/',views.delete_task,name='delete_task'),
+
+    path('',views.task_page,name='task_page'),
+    path('task_add/',views.task_add,name='task_add'),
+    path('edit_taskid/<int:id>/',views.edit_taskid,name='edit_taskid'),
+    path('delete_taskid/<int:id>/',views.delete_taskid,name='delete_taskid'),
+    path('filter_tasks_by_priority',views.filter_tasks_by_priority,name='filter_tasks_by_priority'),
+    
 ]
--- myproject/taskapp/views.py
+++ myproject/taskapp/views.py
@@ -2,36 +2,88 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import *
 from .forms import TaskForm
 
-def view_all_tasks(request):
+# def view_all_tasks(request):
+#     tasks = Task.objects.all()
+#     return render(request,'task_list.html',{'tasks':tasks}) 
+
+# def add_task(request):
+#     if request.method == "POST":
+#         form = TaskForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('view_all_tasks')
+#     else:
+#         form = TaskForm()     
+#     return render(request,'task_form.html',{'form':form})    
+
+# def edit_task(request, task_id):
+#     task = get_object_or_404(Task,id=task_id)
+#     if request.method == "POST":
+#         form = TaskForm(request.POST, instance=task)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('view_all_tasks')
+#     else:
+#         form = TaskForm(instance=task)
+#     return render(request,'task_form.html',{'form':form})
+
+# def delete_task(request,task_id):
+#     task = get_object_or_404(Task, id=task_id)
+#     task.delete()
+#     return redirect('view_all_tasks')
+
+def task_page(request):
     tasks = Task.objects.all()
-    return render(request,'task_list.html',{'tasks':tasks}) 
+    return render(request,'task_page.html',{'tasks':tasks})
 
-def add_task(request):
+def task_add(request):
     if request.method == "POST":
-        form = TaskForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('view_all_tasks')
-    else:
-        form = TaskForm()     
-    return render(request,'task_form.html',{'form':form})    
+        title = request.POST['title']
+        description = request.POST['description']
+        priority = request.POST['priority']
+        status = request.POST['status']
+        
+        Task.objects.create(title=title,description=description,priority=priority,status=status)
 
-def edit_task(request, task_id):
-    task = get_object_or_404(Task,id=task_id)
+        return redirect('task_page')
+    
+    else:
+        return render(request,'task_add.html')
+
+def edit_taskid(request,id):
+    task = Task.objects.get(id=id)
     if request.method == "POST":
-        form = TaskForm(request.POST, instance=task)
-        if form.is_valid():
-            form.save()
-            return redirect('view_all_tasks')
-    else:
-        form = TaskForm(instance=task)
-    return render(request,'task_form.html',{'form':form})
+        title = request.POST['title']
+        description = request.POST['description']
+        priority = request.POST['priority']
+        status = request.POST['status']
 
-def delete_task(request,task_id):
-    task = get_object_or_404(Task, id=task_id)
+        task.title = title
+        task.description = description
+        task.priority = priority
+        task.status = status
+
+        task.save()
+        return redirect('task_page')
+    else:
+        return render(request,'edit_taskid.html',{'task':task})
+    
+def delete_taskid(request,id):
+    task = Task.objects.get(id=id)
     task.delete()
-    return redirect('view_all_tasks')
+    return redirect('task_page')
 
+def filter_tasks_by_priority(request):
+    if request.method == 'POST':
+        priority = request.POST.get('priority')  # Get the priority value from the form (POST data)
 
-def task1(request):
-    return render(request,'task1.html')
+        if priority:
+            tasks = Task.objects.filter(priority=priority)  # Filter tasks by the selected priority
+        else:
+            tasks = Task.objects.all()  # If no priority is selected, return all tasks
+
+        # Redirect to another HTML page after filtering
+        return render(request, 'priority.html', {'tasks': tasks, 'selected_priority': priority})
+    
+    # If it's a GET request, just show the form without filtering
+    return render(request, 'task_page.html')
